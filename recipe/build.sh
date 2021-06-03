0496c115cdc95ce934eb8afe48b4d7c8c2b6796f
Serhii Kupriienko <skupriienko@anaconda.com>
2021-06-03 13:52:48 +0300

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,26 +1,14 @@
 #!/bin/bash
 
-if [[ $(uname) == Darwin ]]; then
-  RPATH="-rpath ${PREFIX}/lib"
-elif [[ $(uname) == Linux ]]; then
-  RPATH="-Wl,-rpath,${PREFIX}/lib"
-fi
-
-### Build
-CXXFLAGS="${CXXFLAGS} "-I${PREFIX}/include \
-LDFLAGS="${LDFLAGS} -L${PREFIX}/lib ${RPATH}" \
-
-mkdir -p build
-cd build
-
-cmake  \
-   -DCMAKE_BUILD_TYPE=Release \
-   -DCMAKE_INSTALL_PREFIX=$PREFIX \
-   -DCMAKE_INSTALL_LIBDIR=$PREFIX/lib \
-   -DBUILD_SHARED_LIBS=ON \
-   -DCMAKE_AR=${AR} \
-   -DCMAKE_RANLIB=${RANLIB} \
-   ..
-make -j ${CPU_COUNT}
-make install
-
+mkdir build
+pushd build
+cmake \
+  -GNinja \
+  -DBUILD_SHARED_LIBS=ON \
+  -DLEVELDB_BUILD_TESTS=OFF \
+  -DLEVELDB_BUILD_BENCHMARKS=OFF \
+  -DCMAKE_INSTALL_PREFIX=$PREFIX \
+  -DCMAKE_INSTALL_LIBDIR=$PREFIX/lib \
+  ..
+ninja install
+popd
